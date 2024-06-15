bb6ee67396c8072e56cd4705a6c3c7efd92da845
trantuvan <trantuvan.kan@gmail.com>
2024-06-15 13:53:10 +0700

    refactor introduce constant

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
 	fmt.Println(Hello("word"))
 }
 
 func Hello(name string) string {
-	return fmt.Sprint("Hello, " + name)
+	return fmt.Sprint(englishHelloPrefix + name)
 }
