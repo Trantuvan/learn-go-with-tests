6ca9f0fbd18ccb38a87253764540f4d15054e5ae
trantuvan <trantuvan.kan@gmail.com>
2024-06-22 12:37:43 +0700

    add subtest to show sum func could take any size of array

--- array/array_test.go
+++ array/array_test.go
@@ -1,13 +1,27 @@
 package array
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [6]int{1, 2, 3, 4, 5, 10}
-	got := Sum(numbers[:]...)
-	want := 25
+	t.Run("collection of 6 numbers", func(t *testing.T) {
+		numbers := [6]int{1, 2, 3, 4, 5, 10}
+		got := Sum(numbers[:]...)
+		want := 25
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers...)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
