fb4d7f7feaadce368dfb83e36fcf281b5ca3b0e6
trantuvan <trantuvan.kan@gmail.com>
2024-06-26 21:34:03 +0700

    impl add unique word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,7 +4,10 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrorNotFound = "could not find the word you were looking for"
+var (
+	ErrorNotFound  = "could not find the word you were looking for"
+	ErrorWordExist = "cannot add word because it already exists"
+)
 
 func (d Dictionary) Search(key string) (string, error) {
 	if val, ok := d[key]; ok {
@@ -13,6 +16,12 @@ func (d Dictionary) Search(key string) (string, error) {
 	return "", errors.New(ErrorNotFound)
 }
 
-func (d Dictionary) Add(key, val string) {
-	d[key] = val
+func (d Dictionary) Add(key, val string) error {
+	_, err := d.Search(key)
+
+	if err != nil {
+		d[key] = val
+		return nil
+	}
+	return errors.New(ErrorWordExist)
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -23,17 +23,31 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("test", "this is just a test")
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
 
-	if err != nil {
-		t.Fatalf("should find this added word %q", err)
-	}
+		if err != nil {
+			t.Fatalf("should find this added word %q", err)
+		}
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+		dictionary.Add("test", "this is just a test")
+
+		got, err := dictionary.Search("test")
+
+		if err != nil {
+			t.Fatalf("should find this added word %q", err)
+		}
+
+		assertStrings(t, got, "this is just a test")
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
