3073a97f4d31fe37109d1baf41f94f572fa696ff
trantuvan <trantuvan.kan@gmail.com>
2024-06-30 11:37:29 +0700

    Refactor write to buffer from 1..3

--- mockings/countdown.go
+++ mockings/countdown.go
@@ -6,5 +6,13 @@ import (
 )
 
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	if out == nil {
+		return
+	}
+	for i := 3; i >= 1; i-- {
+		fmt.Fprintln(out, i)
+		if i == 1 {
+			fmt.Fprint(out, "Go!")
+		}
+	}
 }
