2d76781b1dbadd855af1d8150776a8e071ebff34
trantuvan <trantuvan.kan@gmail.com>
2024-07-02 22:39:14 +0700

    using channel to process multi checkWebsite

--- concurrency/check_website.go
+++ concurrency/check_website.go
@@ -1,17 +1,25 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChan := make(chan result)
 
 	for _, url := range urls {
 		go func(url string) {
-			results[url] = wc(url)
+			resultChan <- result{url, wc(url)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChan
+		results[result.string] = result.bool
+	}
+
 	return results
 }
