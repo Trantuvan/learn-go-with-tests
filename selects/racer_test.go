96898d0d264a855cd1028cf7d76005bb80a3cf40
trantuvan <trantuvan.kan@gmail.com>
2024-07-13 18:24:44 +0700

    Using httptest package
    Notice:
     Set up fake server, each of which returns its own
     url -> Racer using these url make a get request
     This is how the TestRacer knows exactly which is
     the faster ones.

--- selects/racer_test.go
+++ selects/racer_test.go
@@ -1,15 +1,31 @@
 package selects
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.google.com"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Millisecond * 20)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
-	got := Racer(slowURL, fastURL)
+	got := Racer(fastURL, slowURL)
 
 	if want != got {
 		t.Errorf("got %q, want %q", got, want)
 	}
+	slowServer.Close()
+	fastServer.Close()
 }
