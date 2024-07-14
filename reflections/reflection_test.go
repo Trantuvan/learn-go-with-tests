baf1c38edb9cfb9645fad2a5e2183ebe9afb78fa
trantuvan <trantuvan.kan@gmail.com>
2024-07-14 15:44:44 +0700

    Refactor test to table based test

--- reflections/reflection_test.go
+++ reflections/reflection_test.go
@@ -1,26 +1,33 @@
 package reflections
 
 import (
+	"slices"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{{
+		"struct with one string field",
+		struct {
+			Name string
+		}{"Chris"},
+		[]string{"Chris"},
+	}}
 
-	x := struct {
-		Name string
-	}{expected}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(s string) {
+				got = append(got, s)
+			})
 
-	Walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
-	}
-
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+			if !slices.Equal(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
